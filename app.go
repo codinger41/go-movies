1c35d8d7950e42050cc104913dc4b643b568a80b
leksyib <leksyib14@gmail.com>
2019-04-20 16:41:44 +0100

    change dependency path

--- app.go
+++ app.go
@@ -8,9 +8,9 @@ import (
 	"gopkg.in/mgo.v2/bson"
 
 	"github.com/gorilla/mux"
-	. "github.com/mlabouardy/movies-restapi/config"
-	. "github.com/mlabouardy/movies-restapi/dao"
-	. "github.com/mlabouardy/movies-restapi/models"
+	. "github.com/leksyib/go-movies/config"
+	. "github.com/leksyib/go-movies/dao"
+	. "github.com/leksyib/go-movies/models"
 )
 
 var config = Config{}
